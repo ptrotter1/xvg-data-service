def90890904a2f6ae15b0e1cb21415153e82fc1b
Paul Trotter <paul@trottercode.com>
2018-05-03 22:06:38 -0500

    Get YiiMP stats for first address in list

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function numberWithCommas(x) {
     var parts = x.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
-  }
+}
 
 function getTotalBalance() {
     return new Promise(async (resolve, reject) => {
@@ -36,16 +36,32 @@ function getValue() {
     });
 }
 
+function getYiimpInfo() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let response = await axios.get(`http://api.yiimp.eu/api/wallet?address=${config.wallets[0]}`);
+            resolve(response.data);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
 (async () => {
     let outArray = [];
+    let outArray2 = [];
     let decimals = 6;
     let padStart = 0;
+    let padStart2 = 0;
 
     https.globalAgent.options.rejectUnauthorized = false;
 
     try {
+        xvgMiningStats = await getYiimpInfo();
         xvgBalance = await getTotalBalance();
         xvgValue = await getValue();
+
+        console.log("\n= Balance ==============");
 
         outArray.push(`${numberWithCommas(xvgBalance.toFixed(decimals))} XVG`);
         Object.keys(xvgValue).forEach((key, index) => 
@@ -53,7 +69,23 @@ function getValue() {
         );
 
         for (outStr of outArray) if (outStr.length > padStart) padStart = outStr.length;
-        for (outStr of outArray) console.log(outStr.padStart(padStart));
+        for (outStr of outArray) {
+            if (outStr.indexOf("XVG") == -1) {
+                console.log("≅ " + outStr.padStart(padStart));  
+            } else {
+                console.log("  " + outStr.padStart(padStart));  
+            }
+        } 
+
+        console.log("\n= Current Rate =========");
+
+        outArray2.push(`${numberWithCommas(xvgMiningStats.paid24h.toFixed(decimals))} XVG/24h`);
+        outArray2.push(`${numberWithCommas((xvgMiningStats.paid24h * xvgValue["USD"]).toFixed(decimals))} USD/24h`);
+
+        for (outStr of outArray2) if (outStr.length > padStart2) padStart2 = outStr.length;
+        for (outStr of outArray2) console.log("  " + outStr.padStart(padStart2));
+
+        console.log("");
     } catch (e) {
         throw e;
     }
